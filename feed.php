d6b90f51e345c40c810bdac3eded0a522837413f
Idanord <33676867+Idanord@users.noreply.github.com>
2018-03-08 21:01:35 -0800

    Working on SQL
    Still working on the SQL to display.

--- feed.php
+++ feed.php
@@ -1,66 +1,3 @@
-<?php
-/**
- * index.php is the first page of our Survey Sez applications it's based on
- * demo_list_pager.php along with demo_view_pager.php provides a sample web application
- *
- * The difference between demo_list.php and demo_list_pager.php is the reference to the 
- * Pager class which processes a mysqli SQL statement and spans records across multiple  
- * pages. 
- *
- * The associated view page, demo_view_pager.php is virtually identical to demo_view.php. 
- * The only difference is the pager version links to the list pager version to create a 
- * separate application from the original list/view. 
- * 
- * @package SurveySez
- * @author William Fisher <william.fisher@seattlecenral.edu>
- * @version 0.01 2018/02/06
- * @link http://www.wfdesings.com/
- * @license https://www.apache.org/licenses/LICENSE-2.0
- * @see survey_view.php
- * @see Pager.php 
- * @todo none
- */
-
-# '../' works for a sub-folder.  use './' for the root  
-require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials 
- 
-# SQL statement
-//$sql = "select MuffinName, MuffinID, Price from test_Muffins";
-$sql = 
-"
-select CONCAT(a.FirstName, ' ', a.LastName) AdminName, s.SurveyID, s.Title, s.Description, 
-date_format(s.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded' from "
-. PREFIX . "surveys s, " . PREFIX . "Admin a where s.AdminID=a.AdminID order by s.DateAdded desc
-";
-
-#Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
-$config->titleTag = 'Surveys made with love & PHP in Seattle';
-
-#Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
-$config->metaDescription = 'Seattle Central\'s ITC250 Class Surveys are made with pure PHP! ' . $config->metaDescription;
-$config->metaKeywords = 'Surveys,PHP,Fun,Regular,Regular Expressions,'. $config->metaKeywords;
-
-//adds font awesome icons for arrows on pager
-$config->loadhead .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
-
-/*
-$config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
-$config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
-$config->metaRobots = 'no index, no follow';
-$config->loadhead = ''; #load page specific JS
-$config->banner = ''; #goes inside header
-$config->copyright = ''; #goes inside footer
-$config->sidebar1 = ''; #goes inside left side of page
-$config->sidebar2 = ''; #goes inside right side of page
-$config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
-$config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
-*/
-
-# END CONFIG AREA ---------------------------------------------------------- 
-
-get_header(); #defaults to theme header or header_inc.php
-
-/*<!--
 <html>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -82,10 +19,8 @@ img{
   padding: 10px !important;
 }
 </style>
--->*/
 
-    
-echo '    
+
 <div class="btn-group dropdown">
   <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Pets
   <span class="caret"></span></button>
@@ -114,8 +49,12 @@ echo '
     <li><a href="feed.php?ID=7">Playstation 4</a></li>
     <li><a href="feed.php?ID=8">PC</a></li>
   </ul>
-</div>';
+</div>
 
+</html>
+
+
+<?php
 session_start();
 
 $database = array("https://news.google.com/news/rss/search/section/q/cats%20-baseball/cats%20-baseball?hl=en&gl=US&ned=us", // Array that hold all the RSS, replace with DB later
--- index.php
+++ index.php
@@ -34,11 +34,11 @@ date_format(s.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded' from "
 ";
 
 #Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
-$config->titleTag = 'Surveys made with love & PHP in Seattle';
+$config->titleTag = 'Feeds made with love & PHP in Seattle';
 
 #Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
-$config->metaDescription = 'Seattle Central\'s ITC250 Class Surveys are made with pure PHP! ' . $config->metaDescription;
-$config->metaKeywords = 'Surveys,PHP,Fun,Regular,Regular Expressions,'. $config->metaKeywords;
+$config->metaDescription = 'Seattle Central\'s ITC250 Class Feeds are made with pure PHP! ' . $config->metaDescription;
+$config->metaKeywords = 'RSS Feeds,PHP,Fun,Regular,Regular Expressions,'. $config->metaKeywords;
 
 //adds font awesome icons for arrows on pager
 $config->loadhead .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
