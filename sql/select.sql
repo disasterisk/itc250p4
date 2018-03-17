159234448d9bdb038faf105f68c72bb854f5ae23
han77x <32688245+han77x@users.noreply.github.com>
2018-03-16 17:38:00 -0700

    edited files.. still needs work.
    edited files comments inside .sql files. Nothing touched in index except line 160.  I feel like the problem is left in the index.php..

--- sql/select.sql
+++ sql/select.sql
@@ -2,9 +2,21 @@
 SELECT * FROM wn18_categories;
 -- get subcategories
 SELECT * FROM wn18_subcategories;
+-- get feeds
+SELECT * FROM wn18_feeds;
 -- get subcategory by category
 SELECT * FROM wn18_subcategories WHERE categoryID = 1;
--- get subcategory's category
+-- get feeds by subcategory
+SELECT * FROM wn18_feeds WHERE subcategoryID = 1;
+-- get feeds from categories
+SELECT * FROM wn18_feeds WHERE categoryID = 1;
+
+
+  
+
+-- (below is the original) did not think the code was needed so commented out. 
+
+/* -- get subcategory's category
 SELECT * FROM wn18_categories c
   JOIN wn18_subcategories s ON categoryID = c.ID
   WHERE s.ID = 1;
@@ -22,4 +34,6 @@ SELECT f.name, f.description, f.url FROM wn18_feeds f
 -- select feed based on subcategory
 SELECT * FROM wn18_feeds f
   JOIN wn18_feed_subcategory fs ON feedID = f.ID
-  WHERE subcategoryID = 1;
+  WHERE subcategoryID = 1;*/
+  
+
